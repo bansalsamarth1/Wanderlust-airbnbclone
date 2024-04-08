cfe1159ea23ced513e6a640e55d446a5a7b731e5
Samarth Bansal <samarthbansal_23me237@dtu.ac.in>
2024-04-09 00:16:13 +0530

    added create,  edit and update route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 main().then(()=>{
@@ -17,17 +18,24 @@ async function main(){
 app.set("view engine" ,"ejs");
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"));
 
 
 app.get("/",function(req,res){
     res.send("This is the root")
 })
 //index route
-app.get("/listing",async (req,res)=>{
+app.get("/listings",async (req,res)=>{
    const allListings = await Listing.find({})
    res.render("listings/index.ejs",{allListings});
     
 })
+
+//new route
+app.get("/listings/new",function(req,res){
+    res.render("listings/new.ejs")
+})
+
 
 //show route
 app.get("/listings/:id",async function(req,res){
@@ -37,8 +45,27 @@ app.get("/listings/:id",async function(req,res){
    res.render("listings/show.ejs",{listing})
 })
 
+//create route
+app.post("/listings",async function(req,res){
+    // let{title,description,image,price,country,location}=req.body;
+ const newListing=   new Listing(req.body.listing);
+ await newListing.save();
+    console.log(newListing)
+    res.redirect("/listings")
+})
 
-
+//edit route
+app.get("/listings/:id/edit",async function(req,res){
+    let {id} = req.params;
+    const listing = await Listing.findById(id)
+    res.render("listings/edit.ejs",{listing});
+})
+//update route
+app.put("/listings/:id",async(req,res)=>{
+let {id}=req.params;
+await Listing.findByIdAndUpdate(id, {...req.body.listing})
+res.redirect("/listings")
+})
 // app.get("/testlisting",async (req,res)=>{
 //     let sampleListing = new Listing({
 //         title:"My new villa",
